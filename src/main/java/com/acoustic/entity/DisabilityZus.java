3610696a8435b2fe5c3950b02a9ddbcab19975f1
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-05 16:53:13 +0200

    modified GenerationType from Sequence to Indentity

--- src/main/java/com/acoustic/entity/DisabilityZus.java
+++ src/main/java/com/acoustic/entity/DisabilityZus.java
@@ -20,7 +20,7 @@ import java.math.BigDecimal;
 @Builder
 public class DisabilityZus {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private BigDecimal disabilityZusAmount;
